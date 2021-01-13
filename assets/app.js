b61fe08a3e6d8b94f4cfb760b5765435f7e113e3
vladsolntsev <vlad.solnt@gmail.com>
2021-01-13 16:34:45 +0100

    bootstrap resolved with jquery

--- assets/app.js
+++ assets/app.js
@@ -7,9 +7,9 @@
 
 // any CSS you import will output into a single css file (app.scss in this case)
 import './styles/app.scss';
-
+const $ = require('jquery');
 // start the Stimulus application
-import './bootstrap';
+require('bootstrap');
 
 require ('../vendor/frvaillant/mapux/Resources/assets/js/map.js')
 require('@fortawesome/fontawesome-free/css/all.min.css');
