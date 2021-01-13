ef79e61f36ba2b43ee8f53ed1fb6070d8588d350
vladsolntsev <vlad.solnt@gmail.com>
2021-01-13 15:18:55 +0100

    entity remake

---
+++ src/Controller/WelcomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WelcomeController extends AbstractController
+{
+
+
+
+}
